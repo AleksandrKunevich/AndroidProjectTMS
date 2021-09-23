f949999688e3dee87bcb7cce2dac1703e1379638
Aleksandr <Kunsanba@gmail.com>
2021-09-23 13:32:49 +0300

    10 add parameter "val calculate: (Int)->String" in sealed class DiscountType

--- app/src/main/java/com/bignerdranch/android/androidprojecttms/DiscountType.kt
+++ app/src/main/java/com/bignerdranch/android/androidprojecttms/DiscountType.kt
@@ -1,12 +1,18 @@
 package com.bignerdranch.android.androidprojecttms
 
-sealed class DiscountType {
-    data class FlatAmount(val value: Int): DiscountType()
-    data class RangeAmount(val startValue: Int, val endValue: Int): DiscountType()
-    object NoDiscount: DiscountType()
+sealed class DiscountType(val calculate:(Int) -> String) {
+    data class FlatAmount(val value: Int) : DiscountType({
+        (it * value).toString()
+    })
+    data class RangeAmount(val startValue: Int, val endValue: Int) : DiscountType({
+        ((startValue + endValue) / 2 * it).toString()
+    })
+    object NoDiscount : DiscountType({
+        "0"
+    })
 
     override fun toString(): String {
-        return when(this) {
+        return when (this) {
             is FlatAmount -> "Flat Amount"
             is RangeAmount -> "Range Amount"
             NoDiscount -> "Object discount type"
