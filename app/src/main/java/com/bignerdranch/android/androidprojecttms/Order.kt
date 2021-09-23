6dd630453735b5634e368f5c141e541a89b475d6
Aleksandr <Kunsanba@gmail.com>
2021-09-23 14:07:49 +0300

    12 add Order class

---
+++ app/src/main/java/com/bignerdranch/android/androidprojecttms/Order.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.androidprojecttms
+
+data class Order(
+    private val idOrder: Int,
+    private val consumer: String,
+    private val product: String,
+    private val address: String,
+    private val price: Double
+) {
+    fun getIdOrder(): Int = idOrder
+    fun getConsumer(): String = consumer
+    fun getProduct(): String = product
+    fun getAddress(): String = address
+    fun getPrice(): Double = price
+}
